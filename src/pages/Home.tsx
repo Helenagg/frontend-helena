7be2b2317bdc730000ce5f32f9e858b4ba10940b
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-02-16 10:52:49 +0100

    created card

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../components/Card'
+
+const Home = () => {
+  return (
+    <div className=''>
+        <Card />
+    </div>
+  )
+}
+
+export default Home
