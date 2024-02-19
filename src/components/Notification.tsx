4f52df2804e4cc706ff2c8136d47381e22e1fe92
Helena <hgonzalezg18@gmail.com>
2024-02-19 16:05:21 +0100

    created component

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 interface NotificationState {
@@ -13,16 +13,17 @@ const Notification: React.FC = () => {
   if (!notification.msg) return null;
 
   const { msg, type } = notification;
-  console.log('type', type)
+
   return (
     <div
-      id="toast-default" className={`flex items-center w-full max-w-xs p-4 text-gray-500 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 ${
+      id='toast-default'
+      className={`flex items-center w-full max-w-xs p-4 text-gray-900 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 ${
         type === 'success' ? 'bg-green-500' : 'bg-red-500'
       }`}
       role='alert'
     >
       <div className='ms-3 text-sm font-normal'>{msg}</div>
-      <button
+      {/* <button
         type='button'
         className='ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700'
         data-dismiss-target='#toast-default'
@@ -44,7 +45,7 @@ const Notification: React.FC = () => {
             d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
           />
         </svg>
-      </button>
+      </button> */}
     </div>
   );
 };
