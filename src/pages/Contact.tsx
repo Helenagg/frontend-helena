2380d808f92841e989df15119e48454926d7cf5d
Helena <hgonzalezg18@gmail.com>
2024-02-20 20:03:48 +0100

    page completed

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+
+export const Contact: FC = () => {
+  return (
+    <div>
+      <div className='container flex flex-col justify-center font-moirai mt-48'>
+        <p className='flex justify-center font-bold text-primary text-5xl'>
+          I'm Helena González González
+        </p>
+        <div className='mt-20'>
+        <p className='flex justify-center font-bold text-primary text-5xl'>
+          I'm FullStack Develper
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact
